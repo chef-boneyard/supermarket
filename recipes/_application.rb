509a188e9995e10636563787dd6be696563c348c
jtimberman <joshua@opscode.com>
2014-06-06 16:31:40 -0600

    mode 0777: not even once

--- recipes/_application.rb
+++ recipes/_application.rb
@@ -37,7 +37,7 @@ deploy_revision node['supermarket']['home'] do
   before_migrate do
     %w(pids log system public).each do |dir|
       directory "#{node['supermarket']['home']}/shared/#{dir}" do
-        mode 0777
+        mode 0755
         recursive true
       end
     end
