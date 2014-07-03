83966fa2c33927c7e6f63482c845cf1f67b5cc45
Christopher Webber <cwebber@getchef.com>
2014-07-03 11:30:12 -0700

    Add the ability to allow a given domain to serve http

--- test/integration/force_ssl/serverspec/app_spec.rb
+++ test/integration/force_ssl/serverspec/app_spec.rb
@@ -2,8 +2,18 @@ require_relative 'spec_helper'
 
 describe 'supermarket' do
 
-  it 'serve Chef Supermarket index web page' do
-    cmd = command 'wget -O - http://localhost 2> /dev/null'
+  it 'redirect to https' do
+    cmd = command 'curl http://localhost 2> /dev/null'
+    expect(cmd.stdout).to match '301 Moved Permanently'
+  end
+
+  it 'serve Chef Supermarket index web page on port 443' do
+    cmd = command 'curl http://localhost:443 2> /dev/null'
+    expect(cmd.stdout).to match '<!DOCTYPE html>'
+  end
+
+  it 'serve Chef Supermarket index web page over http to http-only.example.com' do
+    cmd = command 'curl --header "Host: http-only.example.com" http://localhost 2> /dev/null'
     expect(cmd.stdout).to match '<!DOCTYPE html>'
   end
 
