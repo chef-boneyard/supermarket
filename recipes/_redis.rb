60931cc482b7b6afae3f2df2d49c880a25fc11ca
Stephen Lauck <stephen.lauck@gmail.com>
2014-09-24 16:58:43 -0400

    add back supermarket redis config

--- recipes/_redis.rb
+++ recipes/_redis.rb
@@ -20,4 +20,15 @@
 
 # Redis is required for Sidekiq
 
+node.default['redis']['config']['logfile'] = '/var/log/redis/redis-server.log'
+
 include_recipe 'redis::server'
+
+template '/etc/redis/redis.conf' do
+  source 'redis.conf.erb'
+  owner  'root'
+  group  'root'
+  mode   '0644'
+  notifies :restart, 'service[redis]'
+end
+
