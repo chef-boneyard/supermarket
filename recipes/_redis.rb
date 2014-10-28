d79ff9fe9ae7d8ec32bd60ab28b4836e43b5baa4
Christopher Webber <cwebber@getchef.com>
2014-10-28 09:54:45 -0700

    Move to redisio

--- recipes/_redis.rb
+++ recipes/_redis.rb
@@ -1,15 +1,17 @@
 # Redis is required for Sidekiq
 
-node.default['redis']['config']['logfile'] = '/var/log/redis/redis-server.log'
-
-include_recipe 'redis::server'
-
-if node["platform"] == 'ubuntu' && node["platform_version"].to_f >= 14.04
-  template '/etc/redis/redis.conf' do
-    source 'redis.conf.erb'
-    owner  'root'
-    group  'root'
-    mode   '0644'
-    notifies :restart, 'service[redis]'
-  end
-end
+#node.default['redis']['config']['logfile'] = '/var/log/redis/redis-server.log'
+#
+#include_recipe 'redis::server'
+#
+#if node["platform"] == 'ubuntu' && node["platform_version"].to_f >= 14.04
+#  template '/etc/redis/redis.conf' do
+#    source 'redis.conf.erb'
+#    owner  'root'
+#    group  'root'
+#    mode   '0644'
+#    notifies :restart, 'service[redis]'
+#  end
+#end
+include_recipe 'redisio'
+include_recipe 'redisio::enable'
