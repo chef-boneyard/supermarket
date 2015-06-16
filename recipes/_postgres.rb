f972f42761c299074f93fbbb5a270364ee1b0a67
Anton Koldaev <me@iroller.ru>
2015-06-16 18:02:18 -0500

    Update _postgres.rb
    Use proper order.

--- recipes/_postgres.rb
+++ recipes/_postgres.rb
@@ -45,6 +45,6 @@ end
 
 execute 'postgres[extensions][pg_trgm]' do
   user 'postgres'
-  command "psql -c #{node['postgres']['database']} 'CREATE EXTENSION IF NOT EXISTS pg_trgm'"
+  command "psql #{node['postgres']['database']} -c 'CREATE EXTENSION IF NOT EXISTS pg_trgm'"
   not_if "echo '\dx' | psql #{node['postgres']['database']} | grep pg_trgm", :user => 'postgres'
 end
